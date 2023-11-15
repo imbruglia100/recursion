14797df07b7b43d3d3bb11a0ac09c43da638c61b
Frank <imbruglia100@gmail.com>
2023-11-14 20:59:42 -0500

    On last one

--- problems/04-range.js
+++ problems/04-range.js
@@ -12,9 +12,14 @@ range(7, 6); // []
 
 
 function range(start, end) {
-  // Your code here 
+  debugger
+  if(end <= start) return [];
+  debugger
+  
+  return [].concat( range(start + 1, end) )
 }
 
+console.log(range(1, 5)); // [1, 2, 3, 4]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
